8189a8c0b7da5011325559ef76eec3e8588f6c26
zakir420 <z.farahmand23@gmail.com>
2022-04-13 10:25:56 +0200

    commit

---
+++ test.py
@@ -0,0 +1,40 @@
+import gurobipy as gp
+from gurobipy import GRB
+
+# This example formulates and solves the following simple MIP model :
+# maximize
+# x + y + 2 z
+# subject to
+# x + 2 y + 3 z <= 4
+# x + y >= 1
+# x, y, z binary
+
+try: 
+    m = gp.Model("mip1")
+
+    # variables 
+    x = m.addVar(vtype=GRB.BINARY, name="x")
+    y = m.addVar(vtype=GRB.BINARY, name="y")
+    z = m.addVar(vtype=GRB.BINARY, name="z")
+
+    # set objective 
+    m.setObjective( x + y + 2 * z, GRB.MAXIMIZE)
+
+    # add constraints
+    m.addConstr(x + 2 * y + 3 * z <= 4, "c0")
+    m.addConstr(x + y >= 1, "c1")
+
+    # optimize the model 
+    m.optimize()
+
+    for v in m.getVars():
+        print("%s %g" % (v.varName , v.x))
+        print("Obj: %g" % m.objVal)
+
+except gp.GurobiError as e:
+        print("Error code: " + str(e.errno) + ":" + str(e))
+    
+except AttributeError:
+        print("Encountered an attribute error")
+
+
