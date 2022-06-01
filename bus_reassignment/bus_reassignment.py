f6d2c1b3b68272d4d559ee0669252d30e4b9513b
zakir420 <z.farahmand23@gmail.com>
2022-06-01 14:40:50 +0200

    googlemaps

--- bus_reassignment/bus_reassignment.py
+++ bus_reassignment/bus_reassignment.py
@@ -1,5 +1,5 @@
 # import libraries
-from re import L
+import googlemaps
 import gurobipy as gp
 from gurobipy import GRB
 from gurobipy import multidict, tuplelist, quicksum
@@ -12,11 +12,14 @@ from datetime import datetime
 import random
 import pyodbc
 import datetime as dt
-from datetime import timedelta
+from datetime import timedelta, datetime
 from itertools import combinations, product
 import statistics
 from matplotlib import animation
 from IPython.display import HTML
+import googlemaps
+gmaps = googlemaps.Client(key='AIzaSyAra0o3L3rs-uHn4EpaXx1Y57SIF_02684')
+
 # import dataset
 data = pd.read_csv(
     r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/BezettingFare.csv', sep=';')
@@ -24,6 +27,7 @@ data = pd.read_csv(
 # replace the NaN values with 0
 data['Bezetting'] = data['Bezetting'].fillna(0)
 
+''' Data Pre processing '''
 # departure and passing time to datetime format
 data['date'] = pd.to_datetime(data['IdDimDatum'].astype(str), format='%Y%m%d')
 data['date'] = data.apply(
@@ -42,19 +46,64 @@ data['dep_datetime'] = data['date'].map(
 data['dep_datetime'] = pd.to_datetime(
     data['dep_datetime'], format='%Y-%m-%d %H:%M:%S')
 
+# extract bus trips that depart, pass or arrive to Enschede Central Station
+lines_connected_2enschede = [1, 2, 3, 4, 5, 6, 7, 8, 9, 60, 61, 62, 505, 506]
+data = data[data['PublieksLijnnr'].isin(lines_connected_2enschede)]
 
-# fig = plt.figure(figsize=(12, 10))
-# plt.scatter(data[data['Richting'] == 1]['PublieksLijnnr'],
-#             data[data['Richting'] == 1]['dep_datetime'], c='b')
-# plt.xlabel('Bus Line')
-# plt.ylabel('Departure Time')
-# plt.show()
+
+# to find the deadhead time between the last stop a trip and first stop of another trip,
+first_stop = data.sort_values(by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(
+    subset=['Ritnummer'], keep='first')
+first_stop = first_stop.groupby(['IdDimHalte']).nth(0).reset_index()
+first_stop = first_stop[['IdDimHalte', 'Breedtegraad', 'Lengtegraad']]
+
+last_stop = data.sort_values(by=['PublieksLijnnr', 'passeer_datetime']).drop_duplicates(
+    subset=['Ritnummer'], keep='last')
+last_stop = last_stop.groupby(
+    ['PublieksLijnnr', 'Naam_halte']).nth(0).reset_index()
+last_stop = last_stop[['IdDimHalte', 'Breedtegraad', 'Lengtegraad']]
+
+deadhead_data = []
+for first in first_stop['IdDimHalte']:
+    for last in last_stop['IdDimHalte']:
+        deadhead_data += [[last, first]]
+
+deadhead_data = pd.DataFrame(deadhead_data, columns=[
+                             'last_stop', 'first_stop'])
+deadhead_data = deadhead_data.merge(
+    first_stop, left_on='last_stop', right_on='IdDimHalte')
+deadhead_data.rename(
+    columns={'Breedtegraad': 'lat_last', 'Lengtegraad': 'long_last'}, inplace=True)
+
+deadhead_data = deadhead_data.merge(
+    last_stop, left_on='first_stop', right_on='IdDimHalte')
+deadhead_data.rename(
+    columns={'Breedtegraad': 'lat_first', 'Lengtegraad': 'long_first'}, inplace=True)
+
+
+def calculate_distance(lat1, lon1, lat2, lon2):
+
+    distance = gmaps.distance_matrix([str(lat1) + " " + str(lon1)],
+                                     [str(lat2) + " " + str(lon2)],
+                                     mode='driving')['rows'][0]['elements'][0]['duration']['text'].split(' ')[0]
+    return distance
+
+
+def f(x): return calculate_distance(
+    x['lat_last'], x['long_last'], x['lat_first'], x['long_first'])
+
+
+deadhead_data['travel_time'] = deadhead_data.apply(f, axis=1).astype('float')
+# if two stops are located at the same station, the deadhead time is set to zero
+deadhead_data['travel_time'] = deadhead_data['travel_time'].apply(
+    lambda x: x if x > 1 else 0)
+deadhead_data.drop(['IdDimHalte_x', 'IdDimHalte_y'], axis=1, inplace=True)
 
 
 # %%
 
 # calculate in-vehicle crowd exceeding the capacity threshold
-capacity_threshold = 35
+capacity_threshold = 40
 data["ex_capacity"] = data['Bezetting'].apply(
     lambda x: x - capacity_threshold if (x > capacity_threshold) else 0)
 
@@ -66,17 +115,112 @@ data["ex_capacity"] = data['Bezetting'].apply(
 test_date = {'2022-02-11'}
 test_date = pd.DataFrame(test_date, columns=['date'])
 
-# select the test data set
-test_data = data[data['IdDimDatum'] == 20220211]
 
-''' drop line 8 from the test dataset '''
-test_data = test_data[test_data['PublieksLijnnr'] != 8]
+select_month = 2
+select_day = 11
+data['month'] = pd.to_datetime(data['date']).dt.month
+data['day'] = pd.to_datetime(data['date']).dt.day
+
+# select the test data set
+test_data = data[(data['month'] == select_month) & (data['day'] == select_day)]
+
+# ''' drop line 8 from the test dataset '''
+# test_data = test_data[test_data['PublieksLijnnr'] != 8]
+
+''' Calculating waiting time
+parameters:
+1. headway mean
+2. headway variance '''
+sorted_data = test_data.sort_values(by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(
+    subset=['Ritnummer'], keep='first')
+sorted_data['h_headway'] = sorted_data.groupby(by=['PublieksLijnnr', 'IdDimHalte'])[
+    'dep_datetime'].transform(pd.Series.diff)
+
+sorted_data['h_headway'] = (sorted_data['h_headway'].dt.total_seconds())/60
+sorted_data = sorted_data.sort_values(
+    by=['PublieksLijnnr', 'IdDimHalte', 'dep_datetime'])
+sorted_data['h_headway'].fillna(method='bfill', inplace=True)
+
+# since variance requires at least two values, remove lines with only on values
+sorted_data.groupby(sorted_data.PublieksLijnnr.tolist(), as_index=False).size()
+
+sorted_data['h_var'] = sorted_data.groupby(
+    'PublieksLijnnr')['h_headway'].transform(statistics.variance)
+
+sorted_data['h_mean'] = sorted_data.groupby(
+    'PublieksLijnnr')['h_headway'].transform(statistics.mean)
+
+# select enschede data
+enschede_lines = [1, 2, 3, 4, 5, 6, 7, 9]
+test_enschede = sorted_data[sorted_data['PublieksLijnnr'].isin(enschede_lines)]
+
+''' data pre-processing '''
+
+# ordered list of trips per line
+
+
+# def preprocessing(data):
+#     line_trips = {}
+#     bus_trips = {}
+#     first_stop = {}
+#     last_stop = {}
+#     preprocessing_data = data.sort_values(
+#         by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(subset=['Ritnummer'], keep='first')
+#     line_trips = {k: list(v)
+#                   for k, v in preprocessing_data.groupby('PublieksLijnnr')['Ritnummer']}
+#     bus_trips = {k: list(v)
+#                  for k, v in preprocessing_data.groupby('IdDimVoertuig')['Ritnummer']}
+#     first_stop = {k: v for k, v in zip(
+#         preprocessing_data['Ritnummer'], preprocessing_data['IdDimHalte'])}
+#     last_stop = data.sort_values(by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(
+#         subset=['Ritnummer'], keep='last')
+
+#     last_stop = {k: v for k, v in zip(
+#         last_stop['Ritnummer'], last_stop['IdDimHalte'])}
+
+#     return line_trips, bus_trips, first_stop
+
+
+# preprocessing(test_data)
+
+
+preprocessing_data = test_data.sort_values(
+    by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(subset=['Ritnummer'], keep='first')
+
+lines_dict = {k: list(v)
+              for k, v in preprocessing_data.groupby('PublieksLijnnr')['Ritnummer']}
+
+bus_trips_dict = {k: list(v)
+                  for k, v in preprocessing_data.groupby('IdDimVoertuig')['Ritnummer']}
+
+
+first_stop_dict = {k: v for k, v in zip(
+    preprocessing_data['Ritnummer'], preprocessing_data['IdDimHalte'])}
+
+last_stop = test_data.sort_values(by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(
+    subset=['Ritnummer'], keep='last')
+
+last_stop_dict = {k: v for k, v in zip(
+    last_stop['Ritnummer'], last_stop['IdDimHalte'])}
+
+
+# def get_key_from_value(d, val):
+#     keys = [k for k, v in d.items() if val in v]
+#     if keys:
+#         return keys[0]
+#     return None
+
+# key = get_key_from_value(bus_trips_dict, 42657)
+# print(key)
+
 # test_data['passeer_datetime'] = test_data['passeer_datetime'].apply(conv_time_to_mils)
 # test_data['dep_datetime'] = test_data['dep_datetime'].apply(conv_time_to_mils)
+
 
 # time window-frame of the optimization
 # minimum time frame should be one hour and maximum should be one day
 # lower bound
+
 
 min_time = {'07:20:00'}
 min_time = pd.DataFrame(min_time, columns=['min_time'])
@@ -101,24 +245,6 @@ test_data = test_data[test_data.loc[:, 'dep_datetime'] >= sigma_min[0]]
 test_data = test_data[test_data.loc[:, 'dep_datetime'] <= sigma_max[0]]
 
 
-''' Calculating waiting time
-parameters:
-1. headway mean
-2. headway variance '''
-sorted_data = test_data.sort_values(by=['PublieksLijnnr', 'dep_datetime']).drop_duplicates(
-    subset=['Ritnummer'], keep='first')
-
-sorted_data['h_headway'] = sorted_data.groupby(
-    'PublieksLijnnr')['dep_datetime'].transform(pd.Series.diff)
-sorted_data['h_headway'] = (sorted_data['h_headway'].dt.total_seconds())/60
-sorted_data['h_headway'] = sorted_data['h_headway'].fillna(method='bfill')
-
-sorted_data['h_var'] = sorted_data.groupby(
-    'PublieksLijnnr')['h_headway'].transform(statistics.variance)
-sorted_data['h_mean'] = sorted_data.groupby(
-    'PublieksLijnnr')['h_headway'].transform(statistics.mean)
-
-
 def cal_waiting_time(mean, var):
     waiting_time = mean * 0.5 + 0.5 * (var / mean)
     return waiting_time
@@ -130,10 +256,9 @@ sorted_data['waiting_time'] = cal_waiting_time(
 waiting_time_dict = sorted_data.set_index(
     ['Ritnummer']).to_dict()['waiting_time']
 
+
 # depature time of trips from the first stop
 # # convert datetime to millisecond
-
-
 def conv_time_to_mils(date_time):
     return date_time.timestamp() * 1000
 
@@ -160,6 +285,9 @@ arr_time['passeer_datetime'] = arr_time['passeer_datetime'].apply(
 arr_time_dict = arr_time.set_index(['Ritnummer']).to_dict()['passeer_datetime']
 travel_time_dict = arr_time.set_index(['Ritnummer']).to_dict()['travel_time']
 
+# Set the deadhead time between stops
+
+
 demand_dict = test_data.sort_values(by=['Ritnummer', 'IdDimHalte']).set_index(
     ['Ritnummer', 'IdDimHalte']).to_dict()['Bezetting']
 
@@ -168,6 +296,7 @@ ex_capacity_dict = test_data.sort_values(by=['Ritnummer', 'IdDimHalte']).set_ind
 # list of trips
 trips = test_data['Ritnummer']
 trips = trips.drop_duplicates(keep='first').tolist()
+
 
 # divide the set of of trips into two subsets A and R
 ex_capacity_trip = list(test_data[test_data['ex_capacity'] > 0]['Ritnummer'])
@@ -189,7 +318,7 @@ reAssign = reAssign['Ritnummer'].drop_duplicates(keep='first').tolist()
 
 stops_dict = {k: list(v)
               for k, v in test_data.groupby('Ritnummer')['IdDimHalte']}
-# create the model without function
+# list of following trips for each reAssign trips
 
 
 # def pairs(*lists):
@@ -203,16 +332,21 @@ stops_dict = {k: list(v)
 
 def bus_reassginment(toAssign, reAssign, waitingTime, demand, exceedingCapacity, departureTime, arrivalTime, stops):
     model = gp.Model('Bus Reassignment')
-
+    epsilon = 120000  # this is the time for boarding passengers
     # create pair of potential trips for re-assignemnt and trips that they could be re-assigned before
     paired_trips = tuplelist()
     for i in reAssign:
         for j in toAssign:
-            if arrivalTime[i] <= departureTime[j]:
+            a = first_stop_dict[i]
+            b = last_stop_dict[j]
+            if arr_time_dict[i] + deadhead_dict[(a, b)] * 60000 <= dep_time_dict[j]:
                 paired_trips += [(i, j)]
     # x = model.addVars(trip_pairs, vtype=GRB.BINARY, name='x')
     reassign_var = model.addVars(
         paired_trips, vtype=GRB.BINARY, name="x[%s, %s]" % (i, j))
+    # create pair of potential imposed cancellations
+    imposed_paired = tuplelist()
+
     model.update()
     # add constraints
     model.addConstrs((reassign_var.sum('*', j) <=
@@ -266,10 +400,12 @@ active_arcs(model)
 # list of cancelled trips
 cancelled = [a[0] for a in active_arcs]
 reassigned_before = [a[1] for a in active_arcs]
-
+mos
 
 stranded_pas = quicksum(demand_dict[i, s]
                         for i in cancelled for s in stops_dict[i])
 
 # model.computeIIS()
 # model.write("model.ilp")
+
+''' Plot feasible connection of trips with respect to depature time and XY locations '''
--- bus_reassignment/data.py
+++ bus_reassignment/data.py
@@ -1,30 +1,56 @@
-import pandas as pd 
-import numpy as np 
+from datetime import datetime
+import googlemaps
+import pandas as pd
+import numpy as np
 import datetime as dt
 from datetime import timedelta
-data = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/BezettingFare.csv',
-                    sep=';')
+# data = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/BezettingFare.csv',
+#                     sep=';')
 
-data['date'] = pd.to_datetime(data['IdDimDatum'].astype(str), format='%Y%m%d')
+# data['date'] = pd.to_datetime(data['IdDimDatum'].astype(str), format='%Y%m%d')
 
-data['date'] = data.apply(
-    lambda row: row['date'] + timedelta(1) if row['IdDimTijdBlok']>=24 else row['date'],
-    axis=1)
-# select only enschede lines 
+# data['date'] = data.apply(
+#     lambda row: row['date'] + timedelta(1) if row['IdDimTijdBlok']>=24 else row['date'],
+#     axis=1)
+# select only enschede lines
 enschede_lines = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/Enschede_lines.csv',
-                    sep=';') 
+                             sep=';')
 
-# enschede data 
-enschede_data = pd.merge(data, enschede_lines, on=["IdDimLijnRichting", "IdDimLijnRichting"])
+# enschede data
+enschede_data = pd.merge(data, enschede_lines, on=[
+                         "IdDimLijnRichting", "IdDimLijnRichting"])
 
-# bus stops 
+# bus stops
 bus_stops = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/Twente_bus_stops.csv',
-                    sep=';')
-enschede_data = pd.merge(enschede_data, bus_stops, on=["IdDimHalte", "IdDimHalte"])
+                        sep=';')
+
+
+gmaps = googlemaps.Client(key='AIzaSyAra0o3L3rs-uHn4EpaXx1Y57SIF_02684')
+
+now = datetime.now()
+
+location = []
+for i in len(bus_stops):
+    location = [bus_stops['Lengtegraad'], bus_stops['Breedtegraad']]
+
+
+directions_result = gmaps.directions("52.249605294727324, 6.855930421536205",
+                                     "52.22221149666158, 6.889309051743431",
+                                     mode="driving",
+                                     avoid="ferries",
+                                     departure_time=now
+                                     )
+
+print(directions_result[0]['legs'][0]['distance']['text'])
+print(directions_result[0]['legs'][0]['duration']['text'])
+
+
+enschede_data = pd.merge(enschede_data, bus_stops, on=[
+                         "IdDimHalte", "IdDimHalte"])
 
 # remove unnecessary colums
-enschede_data.drop(['IdFactBezetting', 'IdDimConcessie', 'IdDimModaliteit', 'Bron', 'RitVertrekTijdInt', 'PasseertijdInt', 'HalteVolgNr', 'IdDimOVChipHalte', 'Code_halte', 'Lengtegraad', 'Breedtegraad'], axis=1, inplace=True)
+enschede_data.drop(['IdFactBezetting', 'IdDimConcessie', 'IdDimModaliteit', 'Bron', 'RitVertrekTijdInt', 'PasseertijdInt',
+                   'HalteVolgNr', 'IdDimOVChipHalte', 'Code_halte', 'Lengtegraad', 'Breedtegraad'], axis=1, inplace=True)
 
 
-data.to_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/data_enschede.csv', sep=";")
-
+# data.to_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/data_enschede.csv', sep=";")
