fd3b55264bc6f85fbf25d722d83b628a23e969f2
Zakirfarahmand <78841609+zakirfarahmand@users.noreply.github.com>
2023-02-06 09:26:07 +0100

    Delete Examples directory

--- Examples/Dijkstra.py
+++
@@ -1,7 +0,0 @@
-import gurobipy as gp
-from gurobipy import *
-
-nodes = ['O', 'U','D', 'V']
-origin =['O']
-destination = ['D']
-
--- Examples/game.cs
+++
@@ -1,6 +0,0 @@
-int main() {
-    int n_teams = 4; 
-    
-    print("%d", n_teams); 
-    return 0;
-}
