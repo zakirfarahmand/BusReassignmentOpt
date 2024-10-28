d3238bd613120a322370e12fff6fe324878df67e
Zakir Farahmand <78841609+zakirfarahmand@users.noreply.github.com>
2024-10-28 16:51:53 +0100

    Update data.py

--- bus_reassignment/data.py
+++ bus_reassignment/data.py
@@ -4,16 +4,9 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 from datetime import timedelta
-# data = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/BezettingFare.csv',
-#                     sep=';')
 
-# data['date'] = pd.to_datetime(data['IdDimDatum'].astype(str), format='%Y%m%d')
-
-# data['date'] = data.apply(
-#     lambda row: row['date'] + timedelta(1) if row['IdDimTijdBlok']>=24 else row['date'],
-#     axis=1)
 # select only enschede lines
-enschede_lines = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/Enschede_lines.csv',
+enschede_lines = pd.read_csv(r'[path]',
                              sep=';')
 
 # enschede data
@@ -21,11 +14,11 @@ enschede_data = pd.merge(data, enschede_lines, on=[
                          "IdDimLijnRichting", "IdDimLijnRichting"])
 
 # bus stops
-bus_stops = pd.read_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/Twente_bus_stops.csv',
+bus_stops = pd.read_csv(r'[path]',
                         sep=';')
 
 
-gmaps = googlemaps.Client(key='AIzaSyAra0o3L3rs-uHn4EpaXx1Y57SIF_02684')
+gmaps = googlemaps.Client(key='[key]')
 
 now = datetime.now()
 
@@ -53,4 +46,4 @@ enschede_data.drop(['IdFactBezetting', 'IdDimConcessie', 'IdDimModaliteit', 'Bro
                    'HalteVolgNr', 'IdDimOVChipHalte', 'Code_halte', 'Lengtegraad', 'Breedtegraad'], axis=1, inplace=True)
 
 
-# data.to_csv(r'C:/Users/FarahmandZH/OneDrive - University of Twente/Documenten/PDEng Project/Data/data_enschede.csv', sep=";")
+# data.to_csv(r'[path]', sep=";")
